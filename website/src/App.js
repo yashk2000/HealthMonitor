ab6ebdac97dee921ea7714ec2c49d695b15e2adc
Rohan Rout <rohan.rout98@gmail.com>
2020-12-04 01:09:03 +0530

    :tada: webapp: Setup front-end file structure

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './pages/landingPage'
+
+function App() {
+  return (
+    <div>
+    <LandingPage></LandingPage>  
+    </div>
+  );
+}
+
+export default App;
---
+++ website/src/pages/landingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+function LandingPage(){
+    return(
+        <div>
+            <Button variant="primary">Primary</Button>
+            <h1>HI</h1>
+        </div>
+    )
+}
+
+export default LandingPage;
