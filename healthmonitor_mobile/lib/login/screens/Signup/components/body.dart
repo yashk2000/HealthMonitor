50a23ffa716ff95c5c10024d8fe2f16fe481d5c8
Yash Khare <yashsja@gmail.com>
2020-12-05 14:11:38 +0530

    :rocket: mobile: Add login/signup pages

---
+++ healthmonitor_mobile/lib/login/screens/Signup/components/body.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:healthmonitor_mobile/login/components/already_have_an_account_acheck.dart';
+import 'package:healthmonitor_mobile/login/components/rounded_button.dart';
+import 'package:healthmonitor_mobile/login/components/rounded_input_field.dart';
+import 'package:healthmonitor_mobile/login/components/rounded_password_field.dart';
+import 'package:healthmonitor_mobile/login/screens/Login/login_screen.dart';
+
+import 'background.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "SIGNUP",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
