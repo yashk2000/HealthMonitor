29cde12a6f93818dd0ec3b37cf0923694b4457a3
Yash Khare <yashsja@gmail.com>
2020-12-06 19:01:27 +0530

    :sparkles: mobile: Built the UI for the landing page, about, instructions

---
+++ healthmonitor_mobile/lib/utils/colors.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const primary = Color(0xff212121);
+  static const accent = Color(0xffe16980);
+  static const light = Color(0xffeceff1);
+  static const dark = Color(0xff37474f);
+  static const white = Color(0xfffafafa);
+  static const black = Color(0xff212121);
+  static const heart = Color(0xfff50057);
+  static const bg = Color(0xffe16980);
+  static const teal = Color(0xff00bfa5);
+  static const lightEnd = Color(0xffffa000);
+
+}
+
+class MaterialColors {
+  static const red = Color(0xffd50000);
+  static const blue = Color(0xff2962ff);
+  static const yellow = Color(0xffffd600);
+  static const green = Color(0xff00c853);
+  static const purple = Color(0xffaa00ff);
+  static const pink = Color(0xffc51162);
+  static const pink2 = Color(0xfff1e6ff);
+  static const teal = Color(0xff00bfa5);
+  static const bg = Color(0xffe16980);
+}
+
+class GradientColors {
+  static const lightStart = Color(0xffdd2c00);
+  static const lightEnd = Color(0xffffa000);
+  static const darkStart = Color(0xff304ffe);
+  static const darkEnd = Color(0xff00bcd4);
+}
+
+class ShadowColors {
+  static const light = Color(0x80718792);
+  static const dark = Color(0x801c313a);
+}
