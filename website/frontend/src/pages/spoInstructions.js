a1f5aa3027e5926cf8b7bff478e414d343286723
Rohan Rout <rohan.rout98@gmail.com>
2020-12-13 19:19:37 +0530

    :bug:webapp: Fix camera bug

--- website/frontend/src/pages/spoInstructions.js
+++ website/frontend/src/pages/spoInstructions.js
@@ -5,47 +5,98 @@ import { BGPURPLE } from '../constants';
 import { Camera } from 'react-cam';
 import { SECONDARYPURPLE } from '../constants';
 import { CameraFill } from 'react-bootstrap-icons';
+import axios from 'axios'
+import Webcam from "react-webcam";
+// const axios = require('axios');
+let ans = '';
+let anythingElse;
 
 
-function capture(imgSrc) {
-    console.log('rachit')
-    console.log(imgSrc);
-}
-
-function SPO2Instructions() {
-    const cam = useRef(null);
+// function capture(imgSrc) {
+//     console.log('Image captured!');
+//     imgSrc = imgSrc.replace('data:image/jpeg;base64,', '')
+//     console.log(imgSrc)
+//     for (let i = 0; i < 2; i++) {
+//         imgSrc += `;${imgSrc}`
+//     }
+//     let data = JSON.stringify({ frames: imgSrc });
+//     axios.post(`https://cors-anywhere.herokuapp.com/https://health-monitor-mlh.herokuapp.com/hr`, data, { headers: { "Content-Type": "application/json" } })
+//         .then(response => {
+//             if (typeof (response.data[0]) === Number) {
+//                 anythingElse = toString(response.data).split(" ")
+//                 ans = `heart rate = ${anythingElse[0]} respiratory rate = ${anythingElse[1]}`
+//             } else {
+//                 ans = 'Face not found!'
+//             }
+//             console.log('the ans is', ans)
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });;
+// }
+const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
+   
+function RespiratoryInstructions() {
+    const webcamRef = React.useRef(null);
+   
+    const capture = React.useCallback(
+      () => {
+        let imgSrc = webcamRef.current.getScreenshot();
+        imgSrc = imgSrc.replace('data:image/jpeg;base64,', '')
+        
+        //console.log(imgSrc)
+        for(let i=0; i<2; i++){
+            imgSrc += `;${imgSrc}`
+        }
+        let data = JSON.stringify({ frames: imgSrc });
+        axios.post(`https://cors-anywhere.herokuapp.com/https://health-monitor-mlh.herokuapp.com/spo`, data, { headers: { "Content-Type": "application/json" } })
+            .then(response => {
+                console.log(response.data)
+                if (response.data[0] === 'F') {
+                    ans = 'Oops! It seems that the finger was not recognized!'
+                } else {
+                    ans = `The SpO2 value is ${response.data}`
+                }
+                console.log('the ans is', ans)
+                alert(ans)
+            })
+            .catch(error => {
+                console.log(error);
+            });;
+      },
+      [webcamRef]
+    );
     return (
         <div>
             <div style={BGPURPLE}>
                 <Container className="p-5" >
                     <Col>
                         <Fragment>
-                            <Camera
-                                showFocus={false}
-                                front={false}
-                                capture={capture}
-                                ref={cam}
-                                width="80%"
-                                height="auto"
-                                focusWidth="80%"
-                                focusHeight="60%"
-                                btnColor="white"
+                            <Webcam
+                                audio={false}
+                                height={640}
+                                ref={webcamRef}
+                                screenshotFormat="image/jpeg"
+                                width={860}
+                                videoConstraints={videoConstraints}
                             />
-                            {/* <button onClick={img => cam.current.capture(img)}>Take image</button> */}
                         </Fragment>
                         <div className="d-flex justify-content-center mt-5">
-                            <Button style={{ backgroundColor: SECONDARYPURPLE }} size="lg" active className="rounded" onClick={img => cam.current.capture(img)}>
-                                <CameraFill /> Capture Image
-            </Button>
+                            <Button style={{ backgroundColor: SECONDARYPURPLE }} size="lg" active className="rounded" onClick={capture}>Capture photo</Button>
                         </div>
                     </Col>
                 </Container>
             </div>
             <Card className="mx-5 my-5">
                 <Card.Body>
-                    <Card.Title><strong>SPO2 Calculator Instructions</strong></Card.Title>
+                    <Card.Title><strong>SpO2 Calculator Instructions</strong></Card.Title>
                     <Card.Text>
-                        To measure your SpO2 successfully, read the following instructions carefully: <br /><br />
+                        
+To measure your SpO2 successfully, read the following instructions carefully: <br /><br />
 
                         1. Please sit in an upright position and face the camera.<br />
                         2. Place your finger at a distance of 3-5 cms as shown below.<br />
@@ -58,4 +109,4 @@ function SPO2Instructions() {
     )
 }
 
-export default SPO2Instructions;
+export default RespiratoryInstructions;
